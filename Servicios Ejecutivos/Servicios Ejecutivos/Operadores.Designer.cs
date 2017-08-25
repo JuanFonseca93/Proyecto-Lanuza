1eb26b0aaf7e00185bab4bb58be9eb845773a0ca
Juan <ja.fonseca@utsoe.edu.mx>
2017-08-25 10:08:59 -0500

    mas cambios

--- Servicios Ejecutivos/Servicios Ejecutivos/Operadores.Designer.cs
+++ Servicios Ejecutivos/Servicios Ejecutivos/Operadores.Designer.cs
@@ -36,7 +36,6 @@
             this.txtDireccion = new System.Windows.Forms.TextBox();
             this.txtTelefono = new System.Windows.Forms.TextBox();
             this.txtBuscar = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.dgvOperadores = new System.Windows.Forms.DataGridView();
             this.btnfoto0 = new System.Windows.Forms.Button();
             this.btnBuscar = new System.Windows.Forms.Button();
@@ -52,7 +51,7 @@
             this.txtSaldo = new System.Windows.Forms.Label();
             this.pbxFoto = new System.Windows.Forms.PictureBox();
             this.btncolor = new System.Windows.Forms.Button();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOperadores)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxFoto)).BeginInit();
             this.SuspendLayout();
@@ -129,20 +128,6 @@
             this.txtBuscar.Size = new System.Drawing.Size(186, 20);
             this.txtBuscar.TabIndex = 11;
             this.txtBuscar.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.checkBox1.Location = new System.Drawing.Point(9, 209);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(91, 17);
-            this.checkBox1.TabIndex = 12;
-            this.checkBox1.Text = "Estatus          ";
-            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // dgvOperadores
             // 
@@ -294,27 +279,29 @@
             // 
             // btncolor
             // 
-            this.btncolor.Location = new System.Drawing.Point(110, 208);
+            this.btncolor.Location = new System.Drawing.Point(151, 208);
             this.btncolor.Margin = new System.Windows.Forms.Padding(2);
             this.btncolor.Name = "btncolor";
             this.btncolor.Size = new System.Drawing.Size(16, 17);
             this.btncolor.TabIndex = 28;
             this.btncolor.UseVisualStyleBackColor = true;
             // 
-            // textBox8
+            // button1
             // 
-            this.textBox8.Location = new System.Drawing.Point(239, 207);
-            this.textBox8.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(76, 20);
-            this.textBox8.TabIndex = 29;
+            this.button1.Location = new System.Drawing.Point(24, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "Activo/Inactivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnFoto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(761, 602);
-            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btncolor);
             this.Controls.Add(this.txtSaldo);
             this.Controls.Add(this.label8);
@@ -330,7 +317,6 @@
             this.Controls.Add(this.btnfoto0);
             this.Controls.Add(this.pbxFoto);
             this.Controls.Add(this.dgvOperadores);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.txtBuscar);
             this.Controls.Add(this.txtTelefono);
             this.Controls.Add(this.txtDireccion);
@@ -359,7 +345,6 @@
         private System.Windows.Forms.TextBox txtDireccion;
         private System.Windows.Forms.TextBox txtTelefono;
         private System.Windows.Forms.TextBox txtBuscar;
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.DataGridView dgvOperadores;
         private System.Windows.Forms.PictureBox pbxFoto;
         private System.Windows.Forms.Button btnfoto0;
@@ -375,6 +360,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label txtSaldo;
         private System.Windows.Forms.Button btncolor;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button1;
     }
 }
